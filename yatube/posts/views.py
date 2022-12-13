b88f96e159641ee0e941b508b68b136398ffda9b
tr202 <tr202@ya.ru>
2022-12-13 23:45:50 +0300

    Homework

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,6 @@ from .models import Post, Group
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -20,7 +17,6 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Записи сообщества Лев Толстой – зеркало русской революции.'
     group = get_object_or_404(Group, slug=slug)
     title = 'Записи сообщества ' + group.title
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
